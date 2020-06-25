b722bd199f859887924332aa9df0e4b119133c99
ono shinji <yjsnpi555@gmail.com>
2020-06-25 12:10:57 +0900

    バリデーションのテストを作成し、通るようになった

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   # 下記の記述により、全てのクエリに対して降順で表示される。今回は、処理の早さを考慮してidでソートしてみる。
   default_scope -> { order(created_at: :desc) }
-  validates :task_name,  presence: true, length: { maximum: 100 }
-  validates :content,  presence: true, length: { maximum: 500 }
+  validates :task_name,  presence: true, length: { maximum: 80 }
+  validates :content,  presence: true, length: { maximum: 300 }
 end
