5619f4f6d19116b39e045915214196e5bfc7bb82
ono shinji <yjsnpi555@gmail.com>
2020-06-26 21:54:29 +0900

    バリデーションのテスト完了

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,6 +7,8 @@ class Task < ApplicationRecord
   validates :task_name,  presence: true, length: { maximum: 80 }
   validates :content,  presence: true, length: { maximum: 300 }
   validates :time_limit, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
   enum priority: { 低: 0, 中: 1, 高: 2 }
   enum status: { 未着手: 0, 着手中: 1, 完了: 2 }
 end
