c205208e3c7c0ffe1b71487dc112cc407fccae06
ono shinji <yjsnpi555@gmail.com>
2020-06-29 00:39:49 +0900

    userテーブル作成、taskテーブルと紐付け、シードデータ作成

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 
-ActiveRecord::Schema.define(version: 2020_06_26_071110) do
+ActiveRecord::Schema.define(version: 2020_06_28_150403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -12,7 +12,18 @@ ActiveRecord::Schema.define(version: 2020_06_26_071110) do
     t.datetime "updated_at", null: false
     t.text "content", default: "default_content", null: false
     t.integer "status", default: 0, null: false
+    t.bigint "user_id"
     t.index ["status"], name: "index_tasks_on_status"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "tasks", "users"
 end
